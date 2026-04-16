c8a206ffcd8e5e625118fb2a8af50c5a032d1b36
JokneanKGSousa <joksous4@gmail.com>
2026-04-16 01:09:04 -0300

    UPDT JS Intro- 07 repairs

--- Back-End/Introdução JS/Laços/for/Var contador/Atvdd-03.js
+++ Back-End/Introdução JS/Laços/for/Var contador/Atvdd-03.js
@@ -20,30 +20,48 @@ let prodValorSup = 0;
 let valorMaisCaro = 0;
 let nmProdMaisCaro = "";
 
-alert(`Sistema de Carrinho de Compras
+alert(`--- Sistema de Carrinho de Compras ---\n
         Informe as informações de seu carrinho de compras
     `)
 
 quantCompras = +prompt(`Digite quantos produtos pretende comprar:`)
-for (let i = 1; i <= quantCompras; i++) {
-    nmProduto = prompt(`Digite o nome do ${i}º produto:`);
 
-    valorProduto = +prompt(`Digite o valor do ${i}º produto:`);
-    quantProduto = +prompt(`Informe as unidades compradas do produto:`);
-    valorTotal += valorProduto * quantProduto;
+if (quantCompras <= 0 || quantCompras == isNaN) {
+    alert(`Compra cancelada\n 
+        DIGITE VALORES VÁLIDOS!
+        `);
+} else {
+    for (let i = 1; i <= quantCompras; i++) {
+        nmProduto = prompt(`Digite o nome do ${i}º produto:`);
+        valorProduto = +prompt(`Digite o valor do ${i}º produto:`);
+        quantProduto = +prompt(`Informe as unidades compradas do produto:`);
 
-    if (valorProduto > 100) {
-        prodValorSup += 1;
-    }
+        if (nmProduto && parseInt(valorProduto) > 0 && !isNaN(valorProduto)) {
+            valorTotal += valorProduto * quantProduto;
 
-    if (valorProduto > valorMaisCaro) {
-        valorMaisCaro = valorProduto;
-        nmProdMaisCaro = nmProduto;
+            if (valorProduto > 100) {
+                prodValorSup += 1;
+            }
+
+            if (valorProduto > valorMaisCaro) {
+                valorMaisCaro = valorProduto;
+                nmProdMaisCaro = nmProduto;
+            }
+        } else {
+            alert(`
+                Compra cancelada\n 
+            DIGITE VALORES VÁLIDOS!
+                `)
+            break;
+        }
     }
 }
+
 alert(`
+    ----- Compra Finalizada! -----\n
+    RESUMO DA COMPRA\n
     Quantidade de Produtos Comprados: ${quantCompras} produtos
-    Valor Total da Compra: ${valorTotal.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})}
+    Valor Total da Compra: ${valorTotal.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}
     Quantidade de Produtos Acima de R$100: ${prodValorSup} produtos
-    Produto Mais Caro: ${nmProdMaisCaro} (${valorMaisCaro.toLocaleString("pt-BR", {style: "currency", currency: "BRL"})})
+    Produto Mais Caro: ${nmProdMaisCaro} (${valorMaisCaro.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })})
     `)
--- Back-End/Introdução JS/Laços/while/Atvdd-01.js
+++ Back-End/Introdução JS/Laços/while/Atvdd-01.js
@@ -1,16 +1,27 @@
+let contador = 1;
 let nmNovoAluno = "";
-let anoMatricula = 1;
+let anoMatricula = 0;
+let codMatricula = 0;
 
-codMatricula = 0;
+alert(`--- Sistema de Cadastro da Academia ---\n
+    Preencha os dados necessários para o cadastro.
+    `)
+while (contador <= 10) {
+    nmNovoAluno = prompt(`Insira seu Nome:`);
+    anoMatricula = prompt(`Insira os dois últimos digitos de seu ano de ingresso:`);
 
-if (anoMatricula != isNaN || nmNovoAluno != null) {
-    while (anoMatricula <= 10) {
-        nmNovoAluno = prompt(`Insira seu Nome:`);
-        anoMatricula = prompt(`Insira os dois últimos digitos de seu ano de ingresso:`);
-        codMatricula = anoMatricula++;
+    if (nmNovoAluno && anoMatricula && anoMatricula.length === 2 && !isNaN(anoMatricula) && parseInt(anoMatricula) > 0) {
+        let sufixo = contador.toString().padStart(2, '0');
+        codMatricula = anoMatricula + sufixo;
+        contador++
+        alert(`
+            --- Cadastro Feito Com Sucesso ---\n
+            Aluno: ${nmNovoAluno}\n
+            Seu código de Inscrição é: ${codMatricula}
+            `)
+    } else {
+        alert(`ERRO: Insira nomes e dígitos válidos.`);
+        break;
     }
-    alert(`${nmNovoAluno}`);
-    alert(`${codMatricula}`);
-} else {
-    alert(`ERRO: Insira nomes e dígitos válidos.`);
 }
+alert(`Sistema Encerrado!`)

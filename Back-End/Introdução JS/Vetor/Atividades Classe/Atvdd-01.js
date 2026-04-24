ce3c005dc2270740de3e80b5fc66efbe42665c79
JokneanKGSousa <joksous4@gmail.com>
2026-04-24 16:08:11 -0300

    UPDT JS Intro Vetor- finished

--- Back-End/Introdução JS/Vetor/Atividades Classe/Atvdd-01.js
+++ Back-End/Introdução JS/Vetor/Atividades Classe/Atvdd-01.js
@@ -3,6 +3,5 @@ let modelosCarros = [];
 modelosCarros.unshift("Uno", "HB20", "Argo", "Fit", "Onix");
 
 alert(`
-    Modelos de Carros Disponíveis:\n
-    ${modelosCarros.join("\n")}
+    Modelos de Carros Disponíveis:\n${modelosCarros.join("\n")}
     `);

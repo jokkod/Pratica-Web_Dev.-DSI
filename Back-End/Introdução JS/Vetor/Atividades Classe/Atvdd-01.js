2cc96221802fab057fb77e26bcc6f8926bbf8cb9
JokneanKGSousa <joksous4@gmail.com>
2026-04-24 11:55:07 -0300

    UPDT JS Intro Vetor- 02

--- Back-End/Introdução JS/Vetor/Atividades Classe/Atvdd-01.js
+++ Back-End/Introdução JS/Vetor/Atividades Classe/Atvdd-01.js
@@ -3,10 +3,6 @@ let modelosCarros = [];
 modelosCarros.unshift("Uno", "HB20", "Argo", "Fit", "Onix");
 
 alert(`
-    Carros:\n
-    ${modelosCarros[0]}
-    ${modelosCarros[1]}
-    ${modelosCarros[2]}
-    ${modelosCarros[3]}
-    ${modelosCarros[4]}
-    `)
+    Modelos de Carros Disponíveis:\n
+    ${modelosCarros.join("\n")}
+    `);
--- Back-End/Introdução JS/Vetor/Atividades Classe/Atvdd-02.js
+++ Back-End/Introdução JS/Vetor/Atividades Classe/Atvdd-02.js
@@ -1,34 +1,50 @@
 let modelosCarros = ["Uno", "HB20", "Argo", "Fit", "Onix"];
+let passo = 1;
+let qtddRetirada = 0;
 
-alert(`
-    Carros Removidos:
-    ${modelosCarros[0]}
-    ${modelosCarros[1]}
-    `)
+while(true){
+        if(passo == 1){
+            let entrada = prompt(`--- Catálogo de Carros ---\n
+                Estoque Atual:`+ modelosCarros.join("\n") + 
+                `\n\nPasso 1- Digite quantos itens que deseja retirar:`
+            );
+            qtddRetirada = parseInt(entrada);
+            if (isNaN(qtddRetirada) || qtddRetirada <= 0 || qtddRetirada > modelosCarros.length) {
+            alert(`--- ERRO:\nDigite um número válido (entre 1 e " + estoque.length + ").`);
+            continue;
+            }
+            passo = 2;
+        }
 
-modelosCarros.splice(0, 2);
+        if (passo == 2) {
+            let erro = false;
+            let modelosCarrosCp1 = ["Uno", "HB20", "Argo", "Fit", "Onix"];
 
-alert(`
-    Carros:\n
-    ${modelosCarros[0]}
-    ${modelosCarros[1]}
-    ${modelosCarros[2]}
-    `)
+            for(i == 1; i <= qtddRetirada; i= i + 1){
+            let ItmRetirar = prompt(`Passo 2- Digite o nome do` + i + `º item a ser retirado:`);
+            let atualizacao = modelosCarros.indexOf(ItmRetirar);
 
-let novosCarros = modelosCarros
-for (let i = 1; i <= 3; i++) {
-    novosCarros.push(prompt(`
-        Digite nome do ${i}º novo carro:
-        `));
-};
+            if(atualizacao !== -1) {
+                modelosCarros.splice(atualizacao, 1);
+                alert(`Item(ns):` + ItmRetirar + `removido(s).`);
+            } else {
+                alert("--- ERRO: Item '" + ItmRetirar + "' não encontrado!");
+                modelosCarros = ["Uno", "HB20", "Argo", "Fit", "Onix"];
+                erro = true;
+                break;
+            }
+        }
+
+        if (erro) {
+            passo = 1;
+            continue;
+        }
+        passo = 3;
+    }
+    /* Fazer próx if(passo == 3){} com for incluso contando até 3 e outros if de validação: 
+    1º para verificar erro; e fora dele método push; e 2º para fazer retorno ao passo 1; 
+    e break while fora do if com alerts finais fora do while. */
+}
 
 
-alert(`
-    Carros:\n
-    ${novosCarros[0]}
-    ${modelosCarros[1]}
-    ${modelosCarros[2]}
-    ${modelosCarros[3]}
-    ${modelosCarros[4]}
-    ${modelosCarros[5]}
-    `)
+

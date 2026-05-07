0c93b5ece9ea8c7a5f55e1e6b15eadab3ef47dfa
JokneanKGSousa <joksous4@gmail.com>
2026-05-07 12:24:58 -0300

    UPDT DataAtributte Atividades

--- Back-End/JS Avançado/Projetos/FOKUS/script.js
+++ Back-End/JS Avançado/Projetos/FOKUS/script.js
@@ -4,23 +4,23 @@ const html = document.documentElement /* Trazendo elemento html fora do body */
 const botoes = document.querySelectorAll('.app__card-button[data-contexto]')
 
 botoes.forEach(itemBotoes => {
-    itemBotoes.addEventListener('click', ()=>{
+    itemBotoes.addEventListener('click', () => {
         let contexto = itemBotoes.dataset.contexto
         alterarConteudos(contexto, itemBotoes) // Chamando a função das ações
-    })
-})
+    });
+});
 
 // Função que vai executar as ações do evento de click nos botões
 function alterarConteudos(contexto, botaoClicado){
 
     // Removendo classe ativa dos elementos
-    botoes.forEach(itemBotao => itemBotao.classList.remove('active'))
+    botoes.forEach(itemBotao => itemBotao.classList.remove('active'));
 
     // Adicionando a classe ativa nos elementos
-    botaoClicado.classList.add('active')
+    botaoClicado.classList.add('active');
 
     // Atualizando o data-contexto
-    html.dataset.contexto = contexto
+    html.dataset.contexto = contexto;
 
     
 }
